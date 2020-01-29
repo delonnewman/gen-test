d1a80800ea6258d846ff9deb698eea8b1c187b9b
Delon Newman <delnewman@salud.unm.edu>
2020-01-29 12:32:06 -0700

    started adding specs

--- spec/gen/test_spec.rb
+++ spec/gen/test_spec.rb
@@ -1,9 +1,18 @@
 RSpec.describe Gen::Test do
-  it "has a version number" do
-    expect(Gen::Test::VERSION).not_to be nil
+  include Gen::Test
+
+  AddOne = lambda { |x| x + 1 }
+
+  it 'should increment any integer' do
+    for_all Integer do |a|
+      b = AddOne[a]
+      expect(b).to be(a + 1)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'should raise a TypeError for any string' do
+    for_all String do |a|
+      expect { AddOne[a] }.to raise_error(TypeError)
+    end
   end
 end
